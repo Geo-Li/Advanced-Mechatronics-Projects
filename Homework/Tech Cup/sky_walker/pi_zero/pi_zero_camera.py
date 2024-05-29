02e19315818af39b161ac4ee65b923028f10e2b0
PlsBeHumble <97078554+Geo-Li@users.noreply.github.com>
2024-05-28 22:29:16 -0500

    Added jupyter notebook for pic visualization

--- Homework/Tech Cup/sky_walker/pi_zero/pi_zero_camera.py
+++ Homework/Tech Cup/sky_walker/pi_zero/pi_zero_camera.py
@@ -1,13 +1,12 @@
 import numpy as np
-import scipy.ndimage
 from picamera import PiCamera
-from picamera.array import PiRGBArray
+from picamera.array import PiYUVArray
 
 class LineIdentifier:
     # Define global variables for the class
     iso_settings = [100, 200, 400, 800, 1600]
 
-    def __init__(self, resolution, framerate, color=False, iso_index=3):
+    def __init__(self, resolution, framerate, color = False, iso_index = 3):
         self.camera = PiCamera(resolution=resolution, framerate=framerate)
         if not color:
             self.camera.color_effects = (128, 128)
@@ -16,14 +15,14 @@ class LineIdentifier:
             pass
 
     def capture_image(self):
-        raw_capture = PiRGBArray(self.camera)
+        self.camera.capture("image.jpg")
+        raw_capture = PiYUVArray(self.camera)
         self.camera.capture(raw_capture, format="yuv")
         image = raw_capture.array[:, :, 0]  # Extract the grayscale channel
         return image
 
-    def process_image(self, image):
+    def process_image(self, image, threshold = 128):
         # Apply a binary threshold to get a binary image
-        threshold = 128  # You can adjust this value as needed
         binary_image = (image > threshold).astype(np.uint8)
         
         # Find the x-coordinate of the line center in the bottom half of the image
@@ -40,10 +39,10 @@ class LineIdentifier:
 
 
 if __name__ == "__main__":
-    lineIdentifier = LineIdentifier(resolution=(640, 480), 
-                                    framerate=10, 
-                                    color=False, 
-                                    iso_index=4)
+    lineIdentifier = LineIdentifier(resolution = (640, 480), 
+                                    framerate = 10, 
+                                    color = False, 
+                                    iso_index = 3)
     image = lineIdentifier.capture_image()
     line_position = lineIdentifier.process_image(image)
     print("Detected line position (0-20):", line_position)
